819c358c9ac36ac622e61a0ddb5bb7350369f271
Eric Mauldin <emauldin84@gmail.com>
2019-05-18 14:03:07 -0400

    adjusts db to accept trip_location rather than city, state, county as separate data fields

--- schema.sql
+++ schema.sql
@@ -8,13 +8,11 @@ create table users (
 
 create table trips (
     id serial primary key,
-    trip_city varchar(200),
-    trip_state varchar(200),
-    trip_country varchar(200),
+    trip_location varchar(500),
     trip_date date,
     lat varchar(100),
     lon varchar(100),
     trip_details varchar(8000),
-    trip_photos varchar(200),
+    trip_photos varchar(2000),
     user_id integer references users(id)
 );
