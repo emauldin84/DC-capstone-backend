8685743dc8943192433c84421ac26626fd434ee5
Jonathan Ray <jonathanray17@gmail.com>
2019-05-24 11:34:19 -0400

    fixes unescaped characters from URL and 404 from geocoder

--- controllers/cors.js
+++ controllers/cors.js
@@ -1,9 +1,24 @@
 const axios = require('axios');
+const escape = require('escape-html');
+const encodeUrl = require('encodeurl')
 
 async function handleRequest (req, res){
   const TOKEN = process.env.API_TOKEN
-  let response = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${req.body.location}.json?access_token=${TOKEN}`)
-  res.json({"data" : response.data});
+  if(req.body.location.includes("-")){
+    // intercept the location name and translate/salt the location name to sanitize the name to exclude unsupported characters
+    console.log("Dumb dumb dubrovnik");
+  }
+  if(req.body.location.length > 0){
+    const location = encodeUrl(escape(req.body.location));
+    console.log(location);
+    let response = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?langugae=en&access_token=${TOKEN}`)
+    .then(r => {
+      console.log("did it happen here?");
+      return r
+    })
+    res.json({"data" : response.data});
+  }
+  // res.json({"data" : ['']});
 }
 
 module.exports = {
