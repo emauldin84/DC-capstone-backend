4eeda4745f76c4bf674602116119124f17a358c4
Jonathan Ray <jonathanray17@gmail.com>
2019-05-25 18:55:19 -0400

    allows users to cupload and change profile pictures

--- controllers/photos.js
+++ controllers/photos.js
@@ -1,5 +1,6 @@
 const Photos = require('../models/photos');
 const Trips = require('../models/trips');
+const User = require('../models/users');
 
 async function addPhotos (req, res) {
   //photo url will be passed as parameter in url from react
@@ -30,6 +31,31 @@ async function addPhotos (req, res) {
   });
 }
 
+async function newProfilePhoto (req, res) {
+  //photo url will be passed as parameter in url from react
+  console.log("req.files is" ,req.files);
+  //using express-fileupload - it will b4e in the req.files object
+  if (Object.keys(req.files).length == 0) {
+    return res.json({message:'No files were uploaded.'});
+  }
+  let sampleFile = (req.files.file ? req.files.file : "bad_file_name.xxx");
+  console.log(sampleFile);
+  //get a unique number based on date and trip id:
+  const randomNumber = Math.floor(Math.random() * Math.floor(1000)) // gets random number, 0 - 1000
+  // let fileName = randomNumber.toString() + Date.now().toString() + sampleFile.name;
+  let fileName = `${randomNumber}_${sampleFile.name.split('.')[0]}_${Date.now()}.${sampleFile.name.split('.')[1]}`;
+  sampleFile.mv(`./public/photos/${fileName}`, async function(err) {
+    if (err) {
+        return res.json({message:'error - No files were uploaded.'});
+    }
+    // save the data to the database
+
+    // Need a User method to push filename into db where photo URL should go  
+    const newPic = await User.updateUserPhoto(req.session.user.id, fileName);
+    res.json({newPic});
+  });
+}
+
 async function getUserPhotos(req, res){
 
 }
@@ -37,4 +63,5 @@ async function getUserPhotos(req, res){
   module.exports = {
     addPhotos,
     getUserPhotos,
+    newProfilePhoto,
 };
--- routes/users.js
+++ routes/users.js
@@ -10,12 +10,16 @@ const {
     updateUserByID,
     updatePassworByID
 } = require('../controllers/users');
+const {
+    newProfilePhoto,
+} = require('../controllers/photos');
 
 usersRouter.get('/allusers', getAllUsers);
 usersRouter.get('/email/:email', getUserByEmail);
 // usersRouter.get('/id/:id', getUserById);
 usersRouter.get('/', getUserById);
 usersRouter.post('/', updateUserByID);
+usersRouter.post('/profilepic', newProfilePhoto);
 usersRouter.post('/password', updatePassworByID);
 usersRouter.post('/register', addNewUser);
 
