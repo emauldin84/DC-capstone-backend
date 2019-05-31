e60da4aed7ba8c69f58c9033ef5dfe9a944bf840
Jonathan Ray <jonathanray17@gmail.com>
2019-05-31 10:46:36 -0400

    builds app

---
+++ public/precache-manifest.0693fb182fed2fbbf409b16327e1fa58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d54d58c008ee965ca19f85bac6d0b3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2edaf73eaa37d7079145",
+    "url": "/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "8e289453aec783035f17",
+    "url": "/static/css/main.af34bfe4.chunk.css"
+  },
+  {
+    "revision": "2edaf73eaa37d7079145",
+    "url": "/static/js/2.f4157c4f.chunk.js"
+  },
+  {
+    "revision": "8e289453aec783035f17",
+    "url": "/static/js/main.eaf018a1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
